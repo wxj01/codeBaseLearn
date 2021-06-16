16eb132488a6425ed250b1bed2ad5bdf07207eac
wxj01 <wangxinjian_01@163.com>
2021-06-16 22:32:34 +0800

    自定义阻塞队列

---
+++ src/main/java/com/wxj/codebaselearn/ms/MyBlockingQueue.java
@@ -0,0 +1,107 @@
+package com.wxj.codebaselearn.ms;
+
+import java.util.LinkedList;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ *  面试中第二次被问到
+ */
+public class MyBlockingQueue<T> {
+
+    AtomicInteger count = new AtomicInteger(0);
+    private LinkedList<T> linkedList = new LinkedList<>();
+
+    private  Object lock  = new Object();
+
+    int maxSize ;
+    int minSize = 0;
+
+    public MyBlockingQueue(int maxSize){
+        this.maxSize = maxSize;
+    }
+
+    public int getSize(){
+        return count.get();
+    }
+
+
+    public void put(T t){
+        synchronized (lock){
+            while (count.get() == maxSize){
+                try {
+                    lock.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            linkedList.add(t);
+            count.getAndDecrement();
+            lock.notify();
+        }
+    }
+
+    public T take(){
+        T temp;
+        synchronized (lock){
+            while (count.get() == minSize){
+                try {
+                    lock.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            temp = linkedList.removeFirst();
+            count.decrementAndGet();
+            lock.notify();
+        }
+        return temp;
+    }
+
+
+    public static void main(String[] args) {
+        MyBlockingQueue<String> queue = new MyBlockingQueue<String>(5);
+        queue.put("a");
+        queue.put("b");
+        queue.put("c");
+        queue.put("d");
+        queue.put("e");
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                queue.put("+1");
+                queue.put("+2");
+            }
+        }).start();
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                queue.take();
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                queue.take();
+            }
+        }).start();
+
+    }
+}
