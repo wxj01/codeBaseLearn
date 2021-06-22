f393e70c5c90a1a632ec654db33f63a0421cad2a
wxj01 <269377151@qq.com>
2021-06-22 13:39:54 +0800

    123

--- src/main/java/com/wxj/codebaselearn/ms/WeakReferenceDemo.java
+++ src/main/java/com/wxj/codebaselearn/ms/WeakReferenceDemo.java
@@ -1,6 +1,7 @@
 package com.wxj.codebaselearn.ms;
 
 import java.lang.ref.WeakReference;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author wxj
@@ -14,17 +15,30 @@ public class WeakReferenceDemo {
     public static void main(String[] args) {
         Object o = new Object();
         WeakReference<Object> weakReference = new WeakReference<>(o);
+        WeakReference<Object> weakReference2 = new WeakReference<>("OK");
 
         System.out.println(o);
         System.out.println(weakReference.get());
+        System.out.println(weakReference2.get());
 
-        o = null;
+//        o = null;
         System.gc();
+
+        try {
+            TimeUnit.SECONDS.sleep(30);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
 
         System.out.println("==================================");
 
         System.out.println(o);
         System.out.println(weakReference.get());
+        System.out.println(weakReference2.get());
+
+
+
 
 
         /*结果：
