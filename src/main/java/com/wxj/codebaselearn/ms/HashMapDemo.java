ba56a627c12e3f87c22964baed3a52c9b66193b3
wxj01 <269377151@qq.com>
2021-06-28 16:51:23 +0800

    123

---
+++ src/main/java/com/wxj/codebaselearn/ms/HashMapDemo.java
@@ -0,0 +1,18 @@
+package com.wxj.codebaselearn.ms;
+
+import java.util.HashMap;
+
+/**
+ * @author wxj
+ * @version 1.0
+ * @description: TODO
+ * @date 2021/5/10 0010 18:20
+ */
+public class HashMapDemo {
+    public static void main(String[] args) {
+
+        HashMap<Object, Object> map = new HashMap<>();
+        map.put("aa",1);
+
+    }
+}
