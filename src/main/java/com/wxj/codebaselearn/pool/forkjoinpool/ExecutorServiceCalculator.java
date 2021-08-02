903d6b90f502368bef0898e83d79614b75114efc
wxj_2021 <wangxinjian_01@163.com>
2021-08-02 16:54:16 +0800

    ExecutorService多线程方式实现

---
+++ src/main/java/com/wxj/codebaselearn/pool/forkjoinpool/ExecutorServiceCalculator.java
@@ -0,0 +1,97 @@
+package com.wxj.codebaselearn.pool.forkjoinpool;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.stream.LongStream;
+
+/**
+ * @author wxj
+ * @version 1.0
+ * @description: TODO ExecutorService多线程方式实现
+ * @date 2021/8/2 0002 15:54
+ */
+public class ExecutorServiceCalculator implements Calculator{
+
+    private int parallism;
+    private ExecutorService pool;
+
+    public ExecutorServiceCalculator(){
+        parallism = Runtime.getRuntime().availableProcessors();// CPU的核心数 默认就用cpu核心数了
+        pool = Executors.newFixedThreadPool(parallism);
+    }
+
+    // 处理计算任务线程
+    private static class AddTask implements Callable<Long>{
+
+        private long[] numbers;
+        private int from;
+        private int to;
+
+        public AddTask(long[] numbers, int from, int to) {
+            this.numbers = numbers;
+            this.from = from;
+            this.to = to;
+        }
+
+        @Override
+        public Long call() throws Exception {
+            long total = 0;
+            for (int i = from; i < to; i++) {
+                    total += numbers[i];
+            }
+            return total;
+        }
+    }
+
+
+    @Override
+    public long add(long[] numbers) {
+        List<Future<Long>> results = new ArrayList<>();
+
+        // 把任务分解为 n 份，交给 n 个线程处理   4核心 就等分成4份呗
+        // 然后把每一份都扔个一个SumTask线程 进行处理
+        int part = numbers.length / parallism;
+        for (int i = 0; i < parallism; i++) {
+            int from = i * part; //开始位置
+            int to = (i == parallism - 1) ? numbers.length - 1 : (i + 1) * part - 1; //结束位置
+
+            //扔给线程池计算
+            results.add(pool.submit(new AddTask(numbers, from, to)));
+        }
+
+        // 把每个线程的结果相加，得到最终结果 get()方法 是阻塞的
+        // 优化方案：可以采用CompletableFuture来优化  JDK1.8的新特性
+        long total = 0L;
+        for (Future<Long> f : results) {
+            try {
+                total += f.get();
+            } catch (Exception ignore) {
+            }
+        }
+
+        return total;
+    }
+
+
+//    耗时：19 ms
+//    结果为：49999960000000
+    public static void main(String[] args) {
+        long[] numbers = LongStream.rangeClosed(1, 10000000).toArray();
+        Instant start = Instant.now();
+
+        ExecutorServiceCalculator executorServiceCalculator = new ExecutorServiceCalculator();
+        long results = executorServiceCalculator.add(numbers);
+
+        Instant end = Instant.now();
+
+        System.out.println("耗时："+ Duration.between(start,end).toMillis() + " ms ");
+
+        System.out.println("结果为："+results);
+    }
+}
