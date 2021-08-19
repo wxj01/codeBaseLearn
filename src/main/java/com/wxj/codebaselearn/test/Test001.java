82989925ce25e7b640bedb24eb21df727e980705
wxj_2021 <wangxinjian_01@163.com>
2021-08-19 14:29:24 +0800

    do while 什么鬼

---
+++ src/main/java/com/wxj/codebaselearn/test/Test001.java
@@ -0,0 +1,16 @@
+package com.wxj.codebaselearn.test;
+
+/**
+ * @author wxj
+ * @version 1.0
+ * @description: TODO 什么都不输出，同事问的
+ * @date 2021/8/19 0019 14:27
+ */
+public class Test001 {
+    public static void main(String[] args) {
+        int i = 1;
+        do while(i < 1)
+            System.out.println("i is "+ i);
+        while (i > 1);
+    }
+}
